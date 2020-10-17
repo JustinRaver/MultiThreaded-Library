0928cc6364a6ef8f1c9d027edf8b37f335b95659
Justin Raver <justinraver@u.boisestate.edu>
2020-10-17 14:30:26 -0600

    Added Book Processor class and constructor

---
+++ src/BookProcessor.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BookProcessor {
+    private int numBooksProcessed;
+    private List<Book> booksProcessed;
+
+    public BookProcessor(){
+        numBooksProcessed = 0;
+        booksProcessed = new ArrayList<>();
+    }
+}
