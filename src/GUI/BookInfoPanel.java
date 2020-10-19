1a4f3ad231467d5632be174eca072ec8d9d4ea20
Justin Raver <justinraver@u.boisestate.edu>
2020-10-19 13:31:49 -0600

    Removed invalid file entry field in GUI

--- src/GUI/BookInfoPanel.java
+++ src/GUI/BookInfoPanel.java
@@ -14,7 +14,6 @@ import java.util.Locale;
 public class BookInfoPanel extends JPanel {
     private final JPanel BOOKDATAPANEL;
     private final ActionListener DATALOADLISTENER;
-    private final JTextField FILEENTRYFIELD;
     private final JLabel LINECOUNTFIELD;
     private final JLabel WORDCOUNTFIELD;
     private final JTextArea WORDAREA;
@@ -27,11 +26,9 @@ public class BookInfoPanel extends JPanel {
         JPanel statPanel = new JPanel();
         statPanel.setBorder(BorderFactory.createTitledBorder("Length Info"));
         LINECOUNTFIELD = new JLabel("Total Lines: ");
-//        LINECOUNTFIELD.setPreferredSize(new Dimension(300, 20));
         statPanel.add(LINECOUNTFIELD, BorderLayout.NORTH);
 
         WORDCOUNTFIELD = new JLabel("Total Words: ");
-//        WORDCOUNTFIELD.setPreferredSize(new Dimension(300, 20));
         statPanel.add(WORDCOUNTFIELD,BorderLayout.SOUTH);
         WORDAREA = new JTextArea();
         BOOKDATAPANEL = new JPanel();
@@ -42,22 +39,9 @@ public class BookInfoPanel extends JPanel {
         bookDataScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         bookDataScrollPane.getVerticalScrollBar().setBlockIncrement(100);
 
-        //bookDataScrollPane.add(WORDAREA,BorderLayout.SOUTH);
 
-
-        //This is the invalid file input functionality
-        JPanel invalidWordPanel = new JPanel();
-        invalidWordPanel.setBorder(BorderFactory.createTitledBorder("Invalid Word File"));
-        FILEENTRYFIELD = new JTextField(12);
-        FILEENTRYFIELD.addActionListener(null);
-        invalidWordPanel.add(FILEENTRYFIELD);
-        JButton loadButton = new JButton("Load");
-        loadButton.addActionListener(null);
-        loadButton.setToolTipText("Load your invalid word file");
-        invalidWordPanel.add(loadButton);
         this.add(statPanel,BorderLayout.NORTH);
         this.add(bookDataScrollPane, BorderLayout.CENTER);
-        this.add(invalidWordPanel,BorderLayout.SOUTH);
 
     }
 
